84eadae4bee732fabaa623882f8465e61ae0fc00
RDJulian <jruizd@fi.uba.ar>
2023-03-23 07:57:42 -0300

    feat: Menu del programa.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,13 @@
 
 using namespace std;
 
+void mostrar_menu() {
+    cout << "1. Dar de alta un contacto" << endl;
+    cout << "2. Mostrar todos los contactos" << endl;
+    cout << "3. Mostrar direcciones de memoria de los contactos" << endl;
+    cout << "4. Salir" << endl;
+}
+
 int main() {
     Agenda* agenda = nullptr; //VER SI ESTA LINEA ES CORRECTA//
     try {
@@ -15,6 +22,28 @@ int main() {
         ofstream archivo(RUTA_ARCHIVO);
         return 0;
     }
+
+    int opcion;
+    do {
+        mostrar_menu();
+        cin >> opcion;
+        switch (opcion) {
+            case 1:
+                // COMPLETAR //
+                break;
+            case 2:
+                // COMPLETAR //
+                break;
+            case 3:
+                // COMPLETAR //
+                break;
+            case 4:
+                cout << "Terminando programa." << endl;
+                break;
+            default:
+                cout << "Error." << endl;
+        }
+    } while (opcion != 4);
 
     // COMPLETAR //
 
